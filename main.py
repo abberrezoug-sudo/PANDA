b48a431ee06b9ef8c0594d0fc9ae61c37c0b3409
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-08-06 01:58:28 +0100

    DataFrime

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pandas as pd
 df = pd.read_csv("orders.csv")
 print(df)
+#PRINT THE FIRST 5 ROWS OF THE DATABASE
+print(df.head())
+#PRINT THE LAST 5 ROWS OF THE DATABASE
+print(df.tail())
+#generale information about the database
+print(df.info())
+#generale desccription
+print(df.describe())
+#get just the columns
+print(df.columns)
+#get index of the database
+print(df.index)
+#get by indexe
+print(df["country"])
+print(df[["country","order_id"]])
+#par position
+print(df.iloc[0])
