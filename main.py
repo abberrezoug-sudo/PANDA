0632aef5e6fe2f297efdc58ff81d4f7b5504bf37
abdellah berrezoug <ab.berrezoug@esi-sba.dz>
2026-08-06 02:20:23 +0100

    DataFrimes

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ print(df)
 print(df.head())
 #PRINT THE LAST 5 ROWS OF THE DATABASE
 print(df.tail())
-#generale information about the database
+#General information about the database
 print(df.info())
-#generale desccription
+#General description
 print(df.describe())
 #get just the columns
 print(df.columns)
 #get index of the database
 print(df.index)
-#get by indexe
-print(df["country"])
-print(df[["country","order_id"]])
+#get by index
+print(df["Country"])
+print(df[["Country", "OrderID"]])
 #par position
 print(df.iloc[0])
+print(df["Country"] == "USA")
+#Updating DATA
+df.loc[df["CustomerName"] == "Anna Ivanova", "Product"] = "New Product Name is time"
+df = df.drop(39)
